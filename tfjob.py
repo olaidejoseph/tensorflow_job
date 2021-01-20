1e005f1ef30bbd164449d032d04fe27fc7b0e47e
Josepholaidepetro <73533435+Josepholaidepetro@users.noreply.github.com>
2021-01-20 03:46:22 +0100

    Add files via upload

--- tfjob.py
+++ tfjob.py
@@ -7,100 +7,133 @@ Original file is located at
     https://colab.research.google.com/drive/1Di9ha2wcHThHzn4NN6Fa1pmoqviViwSO
 """
 
-import tensorflow as tf 
-from sklearn.datasets import load_breast_cancer
+from __future__ import absolute_import, division, print_function
 
-# a quick neural network
-
-import numpy as np
-from tensorflow import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation
-from keras.optimizers import SGD, Adam
-from keras.utils import np_utils
-import matplotlib.pyplot as plt
-from keras.layers.core import Dropout
-import logging
-import tempfile
-import sys, os
 import argparse
-np.random.seed(1671)
+import json
+import os
 
-"""Note: The following parameters below are key in the training of the network"""
+import tensorflow_datasets as tfds
+import tensorflow as tf
+from tensorflow.keras import layers, models
 
-# network and training 
-NB_EPOCH = 50
-VERBOSE = 1
-OPTIMIZER = Adam()
-N_HIDDEN = 128
 
-def input_fn():
-  cancer = load_breast_cancer()
-  X = cancer.data
-  y = cancer.target
-  y = np.expand_dims(y,axis=1)
-  x = tf.cast(X, tf.float32)
-  dataset = tf.data.Dataset.from_tensor_slices((x, y))
-  dataset = dataset.repeat(100)
-  dataset = dataset.batch(32)
+def make_datasets_unbatched():
+  BUFFER_SIZE = 10000
 
-  return dataset
+  # Scaling MNIST data from (0, 255] to (0., 1.]
+  def scale(image, label):
+    image = tf.cast(image, tf.float32)
+    image /= 255
+    return image, label
+
+  datasets, _ = tfds.load(name='fashion_mnist', with_info=True, as_supervised=True)
+
+  return datasets['train'].map(scale).cache().shuffle(BUFFER_SIZE)
+
+
+def build_and_compile_cnn_model():
+  model = models.Sequential()
+  model.add(
+      layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+  model.add(layers.MaxPooling2D((2, 2)))
+  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+  model.add(layers.MaxPooling2D((2, 2)))
+  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+  model.add(layers.Flatten())
+  model.add(layers.Dense(64, activation='relu'))
+  model.add(layers.Dense(10, activation='softmax'))
+
+  model.summary()
+
+  model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+
+  return model
+
+
+def decay(epoch):
+  if epoch < 3: #pylint: disable=no-else-return
+    return 1e-3
+  if 3 <= epoch < 7:
+    return 1e-4
+  return 1e-5
+
 
 def main(args):
 
-  model_dir = args.saved_model_dir
+  # MultiWorkerMirroredStrategy creates copies of all variables in the model's
+  # layers on each device across all workers
+  # if your GPUs don't support NCCL, replace "communication" with another
+  strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy(
+      communication=tf.distribute.experimental.CollectiveCommunication.AUTO)
 
-  # Define a Keras Model.
-  NB_CLASSES = 1 
-  OPTIMIZER = Adam()
-  N_HIDDEN = 128
-  DROPOUT = 0.3
+  BATCH_SIZE_PER_REPLICA = 64
+  BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
 
-  model = Sequential()
-  model.add(Dense(N_HIDDEN,input_shape=(30,)))
-  model.add(Activation('relu'))
-  model.add(Dense(N_HIDDEN))
-  model.add(Activation('relu'))
-  model.add(Dense(NB_CLASSES))
-  model.add(Activation('sigmoid'))
+  with strategy.scope():
+    ds_train = make_datasets_unbatched().batch(BATCH_SIZE).repeat()
+    options = tf.data.Options()
+    options.experimental_distribute.auto_shard_policy = \
+        tf.data.experimental.AutoShardPolicy.DATA
+    ds_train = ds_train.with_options(options)
+    # Model building/compiling need to be within `strategy.scope()`.
+    multi_worker_model = build_and_compile_cnn_model()
 
-  model.compile(loss='categorical_crossentropy',optimizer=OPTIMIZER,
-              metrics=['accuracy'])
-  
-  model.summary()
+  # Function for decaying the learning rate.
+  # You can define any decay function you need.
+  # Callback for printing the LR at the end of each epoch.
+  class PrintLR(tf.keras.callbacks.Callback):
 
-  # Define DistributionStrategies and convert the Keras Model to an
-  # Estimator that utilizes these DistributionStrateges.
-  # Evaluator is a single worker, so using MirroredStrategy.
-  
-  strategytrain = tf.distribute.MultiWorkerMirroredStrategy()
-  strategytest = tf.distribute.MirroredStrategy()
-  config = tf.estimator.RunConfig(tf_random_seed=1,
-          train_distribute=strategytrain,
-          eval_distribute=strategytest)
-  keras_estimator = tf.keras.estimator.model_to_estimator(
-      keras_model=model, config=config, model_dir=model_dir)
+    def on_epoch_end(self, epoch, logs=None): #pylint: disable=no-self-use
+      print('\nLearning rate for epoch {} is {}'.format(
+        epoch + 1, multi_worker_model.optimizer.lr.numpy()))
 
-  # Train and evaluate the model. Evaluation will be skipped if there is not an
-  # "evaluator" job in the cluster.
+  callbacks = [
+      tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+      tf.keras.callbacks.LearningRateScheduler(decay),
+      PrintLR()
+  ]
 
-  tf.estimator.train_and_evaluate(
-      keras_estimator,
-      train_spec=tf.estimator.TrainSpec(input_fn=input_fn),
-      eval_spec=tf.estimator.EvalSpec(input_fn=input_fn, ))
+  # Keras' `model.fit()` trains the model with specified number of epochs and
+  # number of steps per epoch. Note that the numbers here are for demonstration
+  # purposes only and may not sufficiently produce a model with good quality.
+  multi_worker_model.fit(ds_train,
+                         epochs=10,
+                         steps_per_epoch=70,
+                         callbacks=callbacks)
+
+  # Saving a model
+  # Let `is_chief` be a utility function that inspects the cluster spec and
+  # current task type and returns True if the worker is the chief and False
+  # otherwise.
+  def is_chief():
+    return TASK_INDEX == 0
+
+  if is_chief():
+    model_path = args.saved_model_dir
+
+  else:
+    # Save to a path that is unique across workers.
+    model_path = args.saved_model_dir + '/worker_tmp_' + str(TASK_INDEX)
+
+  multi_worker_model.save(model_path)
 
 
 if __name__ == '__main__':
-  logger = tf.get_logger()
-  logger.setLevel(logging.INFO)
+  os.environ['NCCL_DEBUG'] = 'INFO'
+
+  # to decide if a worker is chief, get TASK_INDEX in Cluster info
+  tf_config = json.loads(os.environ.get('TF_CONFIG') or '{}')
+  TASK_INDEX = tf_config['task']['index']
+
   parser = argparse.ArgumentParser()
   parser.add_argument('--saved_model_dir',
                       type=str,
                       required=True,
-                      default=os.path.join(tempfile.gettempdir(),
-                           'savedmodel'),
                       help='Tensorflow export directory.')
-  unparsed = parser.parse_known_args()
-  tf.compat.v1.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+
+  parsed_args = parser.parse_args()
+  main(parsed_args)
 
