daa6ed89b731f63177e05603e0f343ecf3fee33e
Josepholaidepetro <73533435+Josepholaidepetro@users.noreply.github.com>
2021-01-23 01:51:35 +0100

    Update tfjob.py

--- tfjob.py
+++ tfjob.py
@@ -44,12 +44,7 @@ def make_datasets_unbatched():
 def model(args):
   model = models.Sequential()
   model.add(
-      layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-  model.add(layers.MaxPooling2D((2, 2)))
-  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-  model.add(layers.MaxPooling2D((2, 2)))
-  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-  model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+      layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)))
   model.add(layers.MaxPooling2D((2, 2)))
   model.add(layers.Conv2D(128, (3, 3), activation='relu'))
   model.add(layers.Flatten())
